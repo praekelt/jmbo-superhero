c5a6d2f5f1c2339cbc2d26e4d2c6c063a5342e34
hedleyroos <hedleyroos@gmail.com>
2015-01-09 14:43:08 +0200

    Simplify admin

--- superhero/admin.py
+++ superhero/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
+from django import forms
 
 from jmbo.admin import ModelBaseAdmin
+
 from superhero.models import Superhero
 
 
-admin.site.register(Superhero, ModelBaseAdmin)
+class SuperheroForm(forms.ModelForm):
+
+    class Meta:
+        model = Superhero
+
+
+class SuperheroAdmin(ModelBaseAdmin):
+    form = SuperheroForm
+
+    def get_fieldsets(self, request, obj=None):
+        result = super(SuperheroAdmin, self).get_fieldsets(request, obj=obj)
+        result = list(result)
+        return tuple([result[0], result[2], result[3]])
+
+
+admin.site.register(Superhero, SuperheroAdmin)
